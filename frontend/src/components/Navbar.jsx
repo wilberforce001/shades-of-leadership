5f61279028fcf80f75a8dcc4e78cfef90808ce43
Kipyegon Wilber <kenmisik@gmail.com>
2025-10-09 15:44:01 +0300

    Logo image

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom'; 
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/logo.jpg';
+
 
 function NavigationBar() {
   return (
-    <Navbar bg='dark' variant='dark' expand='lg'>
+    <Navbar bg='light' expand='lg' className='shadow-sm'>
       <Container>
-        <Navbar.Brand as={Link} to="/">Shades of Leadership</Navbar.Brand>
+        <Navbar.Brand href='/' className='d-flex align-items-center'>
+          <img 
+          src={logo} 
+          alt="Shades of Leadership Logo" 
+          height="50"
+          className='me-2'
+          />
+          <span className='fw-bold text-primary'>Shades of Leadership</span>
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
